8a5e7a4e9cacfe2e9a8aea96db879ad0dd6688e0
unknown <yadalasuryakumar@gmail.com>
2019-11-23 22:11:26 +0530

    files for latest changes

--- src/routes/bills.js
+++ src/routes/bills.js
@@ -1,85 +1,128 @@
-let billModel = require('../models/bill.model')
-let express = require('express')
-let router = express.Router()
+let billModel = require("../models/bill.model");
+let express = require("express");
+let router = express.Router();
 
-router.post('/bill',(req,res)=>{
-    if(!req.body){
-        return res.status(400).send("Request body is missing")
-    }
-    req.body.createdDate= new Date();
+router.post("/bill", (req, res) => {
+  if (!req.body) {
+    return res.status(400).send("Request body is missing");
+  }
+  req.body.createdDate = new Date();
 
-    let model = new billModel(req.body)
-    console.log("user required===",req.body)
-    model.save().then(doc =>{
-        if(!doc || doc.length === 0){
-            res.status(500).send(doc)
-        }
-        res.status(201).send(doc)
+  let model = new billModel(req.body);
+  console.log("user required===", req.body);
+  model
+    .save()
+    .then(doc => {
+      if (!doc || doc.length === 0) {
+        res.status(500).send(doc);
+      }
+      res.status(201).send(doc);
     })
-    .catch(err =>{
-        res.status(500).json(err)
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/bills", (req, res) => {
+  if (!req.query.email) {
+    res.status(400).send("request parameters missing");
+  }
+
+  billModel
+    .find({
+      smart_user: req.query.email
     })
-
+    .then(doc => {
+      if (!doc || doc.length === 0) {
+        res.json({});
+      } else {
+        res.json(doc);
+      }
     })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+router.get("/viewBill", (req, res) => {
+  if (!req.query.id) {
+    res.status(400).send("request parameters missing");
+  }
+  billModel
+    .findOne({
+      _id: req.query.id
+    })
+    .then(doc => {
+      if (!doc || doc.length === 0) {
+        res.json({});
+      } else {
+        res.json(doc);
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
+// Update Bill
+router.post("/updateBill", (req, res) => {
+  if (!req.query.id) {
+    res.status(400).send("request parameters missing");
+  }
+  billModel
+    .findOneAndUpdate(
+      {
+        _id: req.query.id
+      },
+      req.body,
+      { new: true }
+    )
+    .then(doc => {
+      res.json(doc);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+// DElete Bill
+router.post("/deleteBill", (req, res) => {
+  if (!req.query.id) {
+    res.status(400).send("request parameters missing");
+  }
+  billModel
+    .findOneAndRemove({
+      _id: req.query.id
+    })
+    .then(doc => {
+      res.json({ Status: "success" });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+router.get("/profile", (req, res) => {
+  if (!req.query.email) {
+    res.status(400).send("request parameters missing");
+  }
+  userModel
+    .findOne({
+      email: req.query.email
+    })
+    .then(doc => {
+      if (!doc || Object.keys(doc).length === 0) {
+        res.json({});
+      } else {
+        res.json(doc);
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+router.get("/user/:name", (req, res) => {
+  res.send(`you have requested ${req.params.name}`);
+});
+router.get("/error", (req, res) => {
+  throw new Error("This is a forced error");
+});
 
-router.get('/bills', (req,res) =>{
-    if(!req.query.email){
-        res.status(400).send("request parameters missing")
-    }
-
-    billModel.find({
-        "smart_user":req.query.email
-    }).then(doc =>{
-        if(!doc || doc.length===0){
-            res.json({})
-        }else{
-            res.json(doc)
-        }
-        
-    }).catch(err=>{
-        res.status(500).json(err)
-    }) 
-})
-router.get('/viewBill', (req,res)=>{
-if(!req.query.id){
-    res.status(400).send("request parameters missing")
-}
-billModel.findOne({
-    '_id':req.query.id
-}).then(doc =>{
-    if(!doc || doc.length===0){
-        res.json({})
-    }else{
-        res.json(doc)
-    }
-    
-}).catch(err=>{
-    res.status(500).json(err)
-}) 
-})
-router.get('/profile', (req,res) =>{
-    if(!req.query.email){
-        res.status(400).send("request parameters missing")
-    }
-    userModel.findOne({
-        "email":req.query.email
-    }).then(doc =>{
-        if(!doc || Object.keys(doc).length===0){
-            res.json({})
-        }else{
-            res.json(doc)
-        }
-        
-    }).catch(err=>{
-        res.status(500).json(err)
-    }) 
-})
-router.get('/user/:name', (req,res)=>{
-res.send(`you have requested ${req.params.name}`)
-})
-router.get('/error',(req,res)=>{
-    throw new Error("This is a forced error")
-})
-
-module.exports= router;
+module.exports = router;
